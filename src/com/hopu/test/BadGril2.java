4a7cae0e81be1687b079ea7908d4b2bf054c839e
xlzy-xlzy <15188275064@163.com>
2019-12-23 20:59:38 +0800

    第一次提交

---
+++ src/com/hopu/test/BadGril2.java
@@ -0,0 +1,13 @@
+package com.hopu.test;
+
+public class BadGril2 {
+	public void bad(){
+		// TODO Auto-generated method stub
+		System.out.println("you mom");
+	} 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("you father's bad egg");
+	}
+
+}
---
+++ src/com/hopu/test/BadGril3.java
@@ -0,0 +1,13 @@
+package com.hopu.test;
+
+public class BadGril3 {
+	public void bad(){
+		// TODO Auto-generated method stub
+		System.out.println("you mom's a beautiful woman");
+	} 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("you father's bad egg");
+	}
+
+}
